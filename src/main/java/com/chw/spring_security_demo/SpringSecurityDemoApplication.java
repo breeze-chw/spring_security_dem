e033376ee77f2b269cba81c43facf0aef967ca35
chw <che_breeze@163.com>
2022-04-30 19:21:56 +0800

    first commit

---
+++ src/main/java/com/chw/spring_security_demo/SpringSecurityDemoApplication.java
@@ -0,0 +1,18 @@
+package com.chw.spring_security_demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@MapperScan("com.chw.spring_security_demo.mapper")
+public class SpringSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(SpringSecurityDemoApplication.class, args);
+        System.out.println(run);
+
+    }
+
+}
